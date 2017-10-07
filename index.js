45dc4e1dbf552544fb3f8179b4043c5e919ef551
Roel <roel@ralphstarck.com>
2017-10-07 16:17:12 +0200

    add logic

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 require('dotenv').config()
+const PromiseThrottle = require('promise-throttle')
 const sendSMS = require('./sms')
+const results = require('./results.json')
+
+const promiseThrottle = new PromiseThrottle({
+  requestsPerSecond: 0.1, // up to 1 request per second
+  promiseImplementation: Promise
+})
+
+results.forEach((participant, index) => {
+  // console.log('Rank:', index + 1, 'Score:', participant.score, 'Phone:', participant.phone)
+  const rank = index + 1
+  const message = `Morgenavond om 17u (ipv 18u) de voorlaatste speelavond van De Slimste Mens van Beselare. PS: Je staat op plaats ${rank} in de pronostiekwedstrijd voor het winnen van de ballonvaart, zie uitslag: https://goo.gl/twbDfN. Tot dan!`
+  
+  promiseThrottle.add(() => {
+    return sendSMS(participant.phone, message)
+  }).then(data => {
+    console.log(data)
+  })
+})
 
 // --> ADD YOUR LOGIC BELOW
 
 // EXAMPLE:
-sendSMS('0496059773', 'Hello World!')
-  .then(() => {
-    console.log('Message sent!')
-    process.exit()
-  })
-  .catch(() => {
-    console.log('Something went wrong!')
-    process.exit()
-  })
+// sendSMS('0496059773', 'Hello World!')
+//   .then(() => {
+//     console.log('Message sent!')
+//     process.exit()
+//   })
+//   .catch(() => {
+//     console.log('Something went wrong!')
+//     process.exit()
+//   })
--- sms.js
+++ sms.js
@@ -1,7 +1,14 @@
+const blacklist = require('./blacklist.json')
 const axios = require('axios').create({ baseURL: process.env.PHONE_ADDRESS })
 
 module.exports = function sendSMS (phone, message) {
-  return axios.get(`/v1/sms/send?phone=${phone}&message=${message}`)
-    .then(response => Promise.resolve(response.data))
-    .catch(error => Promise.reject(error))
+  if (blacklist.find(blacklistedPhone => blacklistedPhone === phone)) {
+    console.log('~~ BLACKLISTED PHONE ~~')
+    return Promise.resolve({ message, phone })
+  } else {
+    return axios.get(`/v1/sms/send?phone=${phone}&message=${message}`)
+      .then(response => Promise.resolve({ message, phone }))
+      .catch(error => Promise.reject(error))
+    return Promise.resolve({ message, phone })
+  }
 }
